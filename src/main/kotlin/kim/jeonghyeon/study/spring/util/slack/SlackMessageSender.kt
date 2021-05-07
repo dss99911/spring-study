a73d7e57d9b2b5c60fb77a93dbd6b1e75d18dfc0
Hyun <hyun@balancehero.com>
2021-05-07 10:57:26 +0900

    add

--- src/main/kotlin/kim/jeonghyeon/study/spring/util/slack/SlackMessageSender.kt
+++ src/main/kotlin/kim/jeonghyeon/study/spring/util/slack/SlackMessageSender.kt
@@ -5,6 +5,11 @@ import org.springframework.http.HttpEntity
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.postForObject
 
+/**
+ * 여러 채널에 다양하게 전송하고 싶은 경우, incoming-webhook 이라는 slack app찾아보기.
+ * 일반 webhook은 특정 채널에만 전송 가능함
+ *
+ */
 class SlackMessageSender {
     fun sendText(webhookUrl: String, channel: String, userName: String, emoji: String, text: String) {
         val slackData = SlackPostData(channel, userName, emoji, text)
