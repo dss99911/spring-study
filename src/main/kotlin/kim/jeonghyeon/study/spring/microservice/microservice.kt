b91aff60dcbb8a7749093acdd8a88fda4696b23e
Hyun <hyun@balancehero.com>
2022-01-03 15:24:51 +0900

    non-blocking

---
+++ src/main/kotlin/kim/jeonghyeon/study/spring/microservice/microservice.kt
@@ -0,0 +1,6 @@
+//https://lion-king.tistory.com/12
+//eureka server, client가 존재
+//client에서 다른 client를 호출할 때, service 명으로 호출하기만 하면 됨
+//zuul : client중 게이트웨이 역할 (1.x버전에서 blocking, 2.x non-blocking)
+//Spring Cloud Gateway : zuul의 단점 개선
+//    - https://happycloud-lee.tistory.com/218
