27aa950b8c188033fe4bebcad1526fbcf52f2fc0
Hyun <hyun@balancehero.com>
2022-01-03 14:35:34 +0900

    non-blocking

--- src/main/kotlin/kim/jeonghyeon/study/spring/async/NonBlocking.kt
+++ src/main/kotlin/kim/jeonghyeon/study/spring/async/NonBlocking.kt
@@ -2,3 +2,4 @@
 // coroutine처럼 하나의 스레드에서 wait를 하지 않고, thread를 효과적으로 사용하는 방법
 // Callable, DefferedResult
 // WebFlux도 non-blocking
+// 성능 테스트 : https://filia-aleks.medium.com/microservice-performance-battle-spring-mvc-vs-webflux-80d39fd81bf0
