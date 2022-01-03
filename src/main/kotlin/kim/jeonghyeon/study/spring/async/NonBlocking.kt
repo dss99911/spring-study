df24c60170b83aaf82dd3a867363ed874a5fa657
Hyun <hyun@balancehero.com>
2022-01-03 14:20:54 +0900

    non-blockingπ

---
+++ src/main/kotlin/kim/jeonghyeon/study/spring/async/NonBlocking.kt
@@ -0,0 +1,4 @@
+// todo https://warpgate3.tistory.com/entry/NoBlocking-with-Spring-boot
+// coroutine처럼 하나의 스레드에서 wait를 하지 않고, thread를 효과적으로 사용하는 방법
+// Callable, DefferedResult
+// WebFlux도 non-blocking
